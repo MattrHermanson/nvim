94603270b53d8bc7f5dcd38cd0809e3ff4e7cbf9
MattrHermanson <matth2004@outlook.com>
2026-06-21 12:08:35 -0700

    added window keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,29 @@
-
 -- Clear search highlights on pressing <Esc> in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR><Esc>", { desc = "Clear search highlight" })
+
+local opts = { noremap = true, silent = true }
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
+-- Move windows around the layout
+vim.keymap.set("n", "<leader>mH", "<C-w>H", { desc = "Move window to far left" })
+vim.keymap.set("n", "<leader>mJ", "<C-w>J", { desc = "Move window to bottom" })
+vim.keymap.set("n", "<leader>mK", "<C-w>K", { desc = "Move window to top" })
+vim.keymap.set("n", "<leader>mL", "<C-w>L", { desc = "Move window to far right" })
+
+-- Split windows using leader key
+vim.keymap.set("n", "<leader>sh", ":split<CR>", { silent = true, desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { silent = true, desc = "Split window vertically" })
+
+-- Layout equalization and maximizing
+vim.keymap.set("n", "<leader>=", "<C-w>=", { desc = "Equalize window sizes" })
+vim.keymap.set("n", "<leader>zo", ":only<CR>", { desc = "Close all other windows (Zoom)" })
+
+-- Shrink / Grow window width by 5 columns
+vim.keymap.set("n", "<C-,>", ":vertical resize -5<CR>", { silent = true, desc = "Shrink window width" })
+vim.keymap.set("n", "<C-.>", ":vertical resize +5<CR>", { silent = true, desc = "Grow window width" })
+
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,6 @@ vim.opt.softtabstop = 4
 vim.opt.expandtab = true
 
 vim.opt.wrap = false
+
+vim.opt.splitright = true -- Vertical splits open to the right
+vim.opt.splitbelow = true -- Horizontal splits open below
