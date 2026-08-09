1586a5b675a79b5f9cb7369ada4e63d281244f80
MattrHermanson <matth2004@outlook.com>
2026-08-08 17:50:40 -0700

    removed custom everforest bg colors

--- lua/plugins/everforest-nvim.lua
+++ lua/plugins/everforest-nvim.lua
@@ -4,24 +4,26 @@ vim.pack.add({
 
 local contrast = vim.o.background == "dark" and "hard" or "soft"
 
-require("everforest").setup({
-	background = contrast,
-	colours_override = function(palette)
-		palette.bg_dim = "#2b231b"
-		palette.bg0 = "#342e28"
-		palette.bg1 = "#3b3831"
-		palette.bg2 = "#44413a"
-		palette.bg3 = "#4e4b45"
-		palette.bg4 = "#56514b"
-		palette.bg5 = "#5c5b4d"
-		palette.bg_visual = "#593738"
-		palette.bg_red = "#563b35"
-		palette.bg_green = "#494836"
-		palette.bg_blue = "#454b4a"
-		palette.bg_yellow = "#524431"
-		palette.bg_purple = "#533f3d"
-	end,
-})
+-- require("everforest").setup({
+-- 	background = contrast,
+-- 	colours_override = function(palette)
+-- 		palette.bg_dim = "#2b231b"
+-- 		palette.bg0 = "#342e28"
+-- 		palette.bg1 = "#3b3831"
+-- 		palette.bg2 = "#44413a"
+-- 		palette.bg3 = "#4e4b45"
+-- 		palette.bg4 = "#56514b"
+-- 		palette.bg5 = "#5c5b4d"
+-- 		palette.bg_visual = "#593738"
+-- 		palette.bg_red = "#563b35"
+-- 		palette.bg_green = "#494836"
+-- 		palette.bg_blue = "#454b4a"
+-- 		palette.bg_yellow = "#524431"
+-- 		palette.bg_purple = "#533f3d"
+-- 	end,
+-- })
+
+require("everforest").setup({})
 
 vim.api.nvim_create_autocmd("OptionSet", {
 	pattern = "background",
